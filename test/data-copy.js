ae93ee6b0d07a448b1bcebc6fa859bf4ef6fcffc
Raphael Robatsch <raphaelr@f-m.fm>
2012-03-06 09:35:29 +0100

    Test for matching file contents

--- test/data-copy.js
+++ test/data-copy.js
@@ -15,10 +15,26 @@ describe('data copying', function() {
 		});
 	});
 	
+	it('should copy the contents of the root directory file', function(done) {
+		fs.readFile(helper.outfile('data-a.txt'), 'utf8', function(err, data) {
+			if(err) return done(err);
+			expect(data).to.be('a-a-a');
+			done();
+		});
+	});
+	
 	it('should copy files from subdirectories', function(done) {
 		helper.isFile(helper.outfile('subdir/data-b.txt'), function(err, success) {
 			if(err) return done(err);
 			expect(success).to.be.ok();
+			done();
+		});
+	});
+	
+	it('should copy the contents of the subdirectory file', function(done) {
+		fs.readFile(helper.outfile('subdir/data-b.txt'), 'utf8', function(err, data) {
+			if(err) return done(err);
+			expect(data).to.be('b-b-b');
 			done();
 		});
 	});
